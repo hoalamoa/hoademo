0d69c9bc7ce241fcc1b30f3860f7887fd271fb3f
hoalamoa <hoacmgch18105@gmail.com>
2020-05-20 10:36:12 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Profile</h1>
+<p>Name:</p>
+<p>Picture</p>
+<img src="/hoa.img" alt="">
+<a href="https://www.facebook.com/ciaohou/" title="my facebook:"></a>
+</body>
+</html>
