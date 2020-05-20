f48d35ac7fb46ae0d31ee36b021960ce966b6992
hoalamoa <hoacmgch18105@gmail.com>
2020-05-20 10:41:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 <body>
 <h1>Profile</h1>
 <form action="">
-<label>>Name:</label><input type="text">
-<p>Picture</p>
+<label>Name:</label><input type="text">
+<label>Picture:</label>
 <img src="/hoa.img" alt="">
 <a href="https://www.facebook.com/ciaohou/" title="my facebook:"></a>
 </form>
