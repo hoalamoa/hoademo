d2529eac136fa073511d4ea579e19244256dad8c
hoalamoa <hoacmgch18105@gmail.com>
2020-05-20 10:50:19 +0700

    update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <form action="">
 <label>Name:</label><input type="text"><br>
 <label>Picture:</label>
-<img src="/hoa.img" alt=""><br>
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fplo.vn%2Fgiai-tri%2Fxem-nghe-doc%2F1-phu-nu-nga-bi-bat-giu-do-nuoi-qua-nhieu-meo-895483.html&psig=AOvVaw3i0Z98tjLKIOec5b3MRA1u&ust=1590032659718000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJjk4LDDwekCFQAAAAAdAAAAABAP" alt=""><br>
 <a href="https://www.facebook.com/ciaohou/" title="my facebook:">https://www.facebook.com/ciaohou/</a>
 </form>
 </body>
